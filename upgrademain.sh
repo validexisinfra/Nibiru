5dd9dc66848309da32c7ded28093ba76da023cdd
VALIDEXIS <validexisglobal@gmail.com>
2025-05-18 18:53:16 +0300

    Create upgrademain.sh

---
+++ upgrademain.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd $HOME
+rm -rf nibiru
+git clone https://github.com/NibiruChain/nibiru.git
+cd nibiru
+git checkout v2.3.0
+make install
+
+sudo systemctl restart nibid && sudo journalctl -fu nibid -o cat
